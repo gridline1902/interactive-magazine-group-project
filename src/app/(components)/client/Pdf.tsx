58b116d82d2b6d4341258aa1da0c36f5f75b4073
gridline1902 <michaeljrfreeman@gmail.com>
2024-06-14 01:00:30 +0000

    update....

--- src/app/(components)/client/Pdf.tsx
+++ src/app/(components)/client/Pdf.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { Document, Page, pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 import React, { useState } from 'react';
 import HTMLFlipBook from 'react-pageflip';
 
