474fa20671b866bc3b537c75384339b8b7a8494e
gridline1902 <michaeljrfreeman@gmail.com>
2024-06-14 00:58:09 +0000

    update....

--- src/app/(components)/client/Pdf.tsx
+++ src/app/(components)/client/Pdf.tsx
@@ -1,11 +1,7 @@
 'use client';
 import { Document, Page, pdfjs } from 'react-pdf';
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
-
 import React, { useState } from 'react';
 import HTMLFlipBook from 'react-pageflip';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import 'react-pdf/dist/esm/Page/TextLayer.css';
 
 const Pdf = () => {
     const [numPages, setNumPages] = useState<number>(1);
