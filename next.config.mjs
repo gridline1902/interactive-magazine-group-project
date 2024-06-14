229d1b82a821a64d840eb6a1e0c7b5ee53734506
gridline1902 <michaeljrfreeman@gmail.com>
2024-06-14 00:04:20 +0000

    fifth commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias.canvas = false;
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Add the PDF loader configuration
+    config.module.rules.push({
+      test: /\.pdf$/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            name: "[path][name].[ext]",
+          },
+        },
+        {
+          loader: "pdf-loader",
+        },
+      ],
+    });
+
+    // Ignore the canvas.node file
+    config.module.rules.push({
+      test: /\.node$/,
+      loader: "ignore-loader",
+    });
 
     return config;
   },
